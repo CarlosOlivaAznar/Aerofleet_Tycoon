c77829daaf4a81c5b81c338c094c40cd0f1bb205
CarlosOlivaAznar <carlosoliva5503@gmail.com>
2024-01-06 16:45:08 +0100

    modificacion index sede

--- resources/views/sede/index.blade.php
+++ resources/views/sede/index.blade.php
@@ -46,7 +46,7 @@
           <div class="infodividido margin">
             <p>{{ count($sede->hangar) }}</p>
             <p>1(inop)</p>
-            <p>{{ $sede->aeropuerto->costeOperacional }} / 7.000inop / 15.000inop</p>
+            <p>{{ $sede->aeropuerto->costeOperacional }}</p>
             <p>{{ ($sede->aeropuerto->costeOperacional * 50) * count($sede->hangar) }} / mes</p>
             <p>{{ $sede->ingenieros * 30000 }}€ / mes</p>
             <p>280.000€ / mes(inop)</p>
