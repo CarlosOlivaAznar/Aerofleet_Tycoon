b1211a1664a56dda0d5f01b787145a56a74791cc
CarlosOlivaAznar <carlosoliva5503@gmail.com>
2024-01-12 18:26:57 +0100

    Mostrar espacios totales

--- app/Models/Espacio.php
+++ app/Models/Espacio.php
@@ -21,11 +21,25 @@ class Espacio extends Model
         return $this->numeroDeEspacios - $this->espaciosOcupados();
     }
 
+    // Funcion que devuelve los espacios que estan asignados en una ruta de un usuario
     public function espaciosOcupados()
     {
         $espaciosDep = count(Ruta::where('espacio_departure_id', $this->id)->where('user_id', auth()->id())->get());
         $espaciosArr = count(Ruta::where('espacio_arrival_id', $this->id)->where('user_id', auth()->id())->get());
         return $espaciosDep + $espaciosArr;
+    }
+
+    // Funcion que devuelve los espacios que estan ocupados de un aeropuerto (totales)
+    public function espaciosOcupadosTotales()
+    {
+        $espaciosOcupados = 0;
+        $espacios = Espacio::where('aeropuerto_id', $this->aeropuerto->id)->get();
+        
+        foreach ($espacios as $espacio) {
+            $espaciosOcupados += $espacio->numeroDeEspacios;
+        }
+
+        return $espaciosOcupados;
     }
 
     public function aeropuerto()
