17b9ce47fea9594ab7e99d9a0e3dccbe68db05f3
CarlosOlivaAznar <carlosoliva5503@gmail.com>
2024-01-15 19:22:54 +0100

    Modificacion usuario base de datos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->bigInteger('saldo')->default(200000000);
+            $table->string('ultimaConexion')->default('');
             $table->rememberToken();
             $table->timestamps();
         });
