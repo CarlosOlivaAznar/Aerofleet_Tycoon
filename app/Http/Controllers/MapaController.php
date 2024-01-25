bc8cc90fb902c972832e5b6f420eade96d10f6a1
CarlosOlivaAznar <carlosoliva5503@gmail.com>
2024-01-25 10:43:37 +0100

    Mostrar aviones usuario

--- app/Http/Controllers/MapaController.php
+++ app/Http/Controllers/MapaController.php
@@ -4,11 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Ruta;
+use Carbon\Carbon;
 
 class MapaController extends Controller
 {
     public function index()
     {
-        return view('mapa.index');
+        $rutasUser = Ruta::where('user_id', auth()->id())->get();
+        $avionesVolando = array();
+        // Comprobamos que avion esta ahora mismo en vuelo
+        foreach ($rutasUser as $rutaUser) {
+            $horaInicio = Carbon::createFromFormat('H:i:s', $rutaUser->horaInicio);
+            $horaFin = Carbon::createFromFormat("H:i:s", $rutaUser->horaFin);
+            if(now()->between($horaInicio, $horaFin)){
+                array_push($avionesVolando, $this->calcularPosicion($rutaUser, $horaInicio));
+            }
+        }
+        
+        return view('mapa.index', ['avionesVolando' => $avionesVolando]);
+    }
+
+    public function calcularPosicion($ruta, Carbon $horaInicio)
+    {
+        $latSalida = $ruta->espacio_departure->aeropuerto->latitud;
+        $longSalida = $ruta->espacio_departure->aeropuerto->longitud;
+
+        $latLlegada = $ruta->espacio_arrival->aeropuerto->latitud;
+        $longLlegada = $ruta->espacio_arrival->aeropuerto->longitud;
+        
+        // Diferencia de latitudes y longitudes
+        $difLat = $latSalida - $latLlegada;
+        $difLong = $longSalida - $longLlegada;
+
+        $distanciaRecorrida = intval($horaInicio->diffInMinutes(now()));
+        $tiempoEstimado = explode(":", $ruta->tiempoEstimado); 
+
+        $porcentajeRecorrido = $distanciaRecorrida / ((intval($tiempoEstimado[0]) * 60) + intval($tiempoEstimado[1]));
+
+        // Una vez con el porcentaje de ruta recorrido calculamos la posicion segun la diferencia de posiciones y el porcentaje
+        $difLat *= $porcentajeRecorrido;
+        $difLong *= $porcentajeRecorrido;
+
+        // Calculamos la posicion final del avion
+        $posLat = $latSalida - $difLat;
+        $posLong = $longSalida - $difLong;
+        $maticula = $ruta->flota->matricula;
+        
+        return [$posLat, $posLong, "$maticula"];
     }
 }
--- resources/views/mapa/index.blade.php
+++ resources/views/mapa/index.blade.php
@@ -21,15 +21,36 @@
         </div>
       </div>
 
+      @foreach ($avionesVolando as $avion)
+      <input type="hidden" class="avionesUsuario" value="{{ $avion[0] }}">
+      <input type="hidden" class="avionesUsuario" value="{{ $avion[1] }}">
+      <input type="hidden" class="avionesUsuario" value="{{ $avion[2] }}">
+      @endforeach
+
       <div class="mapa">
         <div id="map"></div>
         <script>
-            var map = L.map('map').setView([41.6647603, -1.0506562], 4);
-  
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
+          // Recuperamos todos los datos de los aviones
+          var domAviones = document.getElementsByClassName("avionesUsuario");
+          console.log(avionesUsuario);
+
+          var avionesUsuario = Array();
+
+          for (var i = 0; i < domAviones.length; i++) {
+            avionesUsuario.push(domAviones[i].value);
+          }
+
+          console.log(avionesUsuario);
+
+
+          var map = L.map('map').setView([41.6647603, -1.0506562], 4);
+
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          }).addTo(map);
+
+            
         </script>
       </div>
     </main>
