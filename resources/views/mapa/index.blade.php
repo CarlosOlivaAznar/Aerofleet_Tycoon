f0ddd3e1cc378519dac6ac15a0fc62b6e9fecc90
CarlosOlivaAznar <carlosoliva5503@gmail.com>
2024-01-25 10:53:47 +0100

    mostrar aviones en el mapa

--- resources/views/mapa/index.blade.php
+++ resources/views/mapa/index.blade.php
@@ -42,6 +42,16 @@
 
           console.log(avionesUsuario);
 
+          // Icono personalizado
+          var planeIcon = L.icon({
+              iconUrl: 'icons/plane-solid-24.png',
+
+              iconSize:     [25, 25], // size of the icon
+              shadowSize:   [0, 0], // size of the shadow
+              iconAnchor:   [12.5, 12.5], // point of the icon which will correspond to marker's location
+              shadowAnchor: [0, 0],  // the same for the shadow
+              popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+          });
 
           var map = L.map('map').setView([41.6647603, -1.0506562], 4);
 
@@ -49,6 +59,12 @@
           maxZoom: 19,
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           }).addTo(map);
+
+          for(var i = 0; i < avionesUsuario.length; i++){
+            L.marker([avionesUsuario[(i*3)], avionesUsuario[(i*3)+1]], {icon: planeIcon}).addTo(map).bindPopup(avionesUsuario[(i*3)+2]);
+          }
+
+          //L.marker([51.5, -0.09], {icon: planeIcon}).addTo(map).bindPopup("prueba");
 
             
         </script>
