ae2a51782e43d18ca448c78740fe63f4a741345b
CarlosOlivaAnar <carlosoliva5503@gmail.com>
2024-03-08 23:23:28 +0100

    Input desplegable con buscador

--- resources/views/home/primerLogin.blade.php
+++ resources/views/home/primerLogin.blade.php
@@ -18,13 +18,62 @@
 
             <div class="campos">
                 <label for="sede">Localizacion de la sede</label>
-                <select name="localizacion" id="localizacion">
-                    <option value="a">El Prat</option>
-                    <option value="b">Madrid Adolfo Suarez Barajas</option>
-                </select>
+
+                <input type="text" class="select" name="sede" id="sede" onfocus="mostrarDd()" onblur="ocultarDd()" onkeyup="filtrar()" placeholder="Busca la localizacion...">
+                <input type="hidden" id="sedeHid" value="">
+                
+                <div class="drop-down" id="dropDown">
+                  <p id="0" onclick="seleccionar(this)">Barcelona El Prat</p>
+                  <p id="1" onclick="seleccionar(this)">Madrid Barajas</p>
+                  <p id="2" onclick="seleccionar(this)">Aeropuerto de Zaragoza</p>
+                  <p id="3" onclick="seleccionar(this)">Aeropuerto de Bilbao</p>
+                </div>
             </div>
         </div>
     </form>
+    <script>
+      function mostrarDd(){
+        // Se muestra el dropDown
+        document.getElementById("dropDown").classList.add("show");
+        // Se ajusta el tamaño de los campos del buscador respecto al ancho del input
+        document.getElementById("dropDown").style.width = String(document.getElementById("sede").offsetWidth) + "px"
+      }
+
+      function ocultarDd(){
+        // Se retrasa la funcion para que deje tiempo para manejar el evento de onclick()
+        setTimeout(function() {
+          // Se oculta el dropDown
+          document.getElementById("dropDown").classList.remove("show");
+        }, 100);
+      }
+
+      function filtrar() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("sede");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("dropDown");
+        a = div.getElementsByTagName("p");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+
+      function seleccionar(element){
+        let input = document.getElementById("sede")
+        let inputHid = document.getElementById("sedeHid")
+
+        input.value = element.innerHTML
+        inputHid.value = element.getAttribute("id");
+
+        ocultarDd();
+        console.log(element);
+      }
+    </script>
   </main>
 </body>
 </html>
