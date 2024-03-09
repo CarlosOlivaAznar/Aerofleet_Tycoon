db88d691328e8846639a804fb6b073d36c8d2089
CarlosOlivaAnar <carlosoliva5503@gmail.com>
2024-03-09 12:41:07 +0100

    Pagina sede modificacion mapa

--- resources/views/sede/index.blade.php
+++ resources/views/sede/index.blade.php
@@ -23,34 +23,49 @@
 
       <div class="informacion">
         <div class="mapaizq">
+          <input type="hidden" id="lat" value="{{ $sede->aeropuerto->latitud }}">
+          <input type="hidden" id="long" value="{{ $sede->aeropuerto->longitud }}">
           <div id="map"></div>
           <script>
-              var map = L.map('map', {zoomControl: false}).setView([41.667787, -1.0376974], 13);
-    
-              L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-              maxZoom: 19,
-              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-              }).addTo(map);
+            let latitud = parseFloat(document.getElementById("lat").value)
+            let longitud = parseFloat(document.getElementById("long").value)
+
+            var map = L.map('map', {zoomControl: false}).setView([latitud, longitud], 12);
+  
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
           </script>
         </div>
         <div class="infodatos">
           <h3>{{ $sede->aeropuerto->nombre }}</h3>
-          <div class="infodividido">
-            <p>Hangares disponibles:</p>
-            <p>Aviones en mantenimiento:</p>
-            <p>Coste operacional:</p>
-            <p>Coste de alquiler:</p>
-            <p>Coste Ingenieros de Mantenimiento:</p>
-            <p>Costes totales:</p>
-          </div>
-          <div class="infodividido margin">
-            <p>{{ count($sede->hangar) }}</p>
-            <p>1(inop)</p>
-            <p>{{ number_format($sede->aeropuerto->costeOperacional, 0, ',', '.') }}</p>
-            <p>{{ number_format(($sede->aeropuerto->costeOperacional * 50) * count($sede->hangar), 0, ',', '.') }} / mes</p>
-            <p>{{ number_format($sede->ingenieros * 30000, 0, ',', '.') }}€ / mes</p>
-            <p>280.000€ / mes(inop)</p>
-          </div>
+          <table class="sinBordes">
+            <tr>
+              <td>Hangares disponibles:</td>
+              <td>{{ count($sede->hangar) }}</td>
+            </tr>
+            <tr>
+              <td>Aviones en mantenimiento:</td>
+              <td>1(inop)</td>
+            </tr>
+            <tr>
+              <td>Coste operacional:</td>
+              <td>{{ number_format($sede->aeropuerto->costeOperacional, 0, ',', '.') }}</td>
+            </tr>
+            <tr>
+              <td>Coste de alquiler:</td>
+              <td>{{ number_format(($sede->aeropuerto->costeOperacional * 50) * count($sede->hangar), 0, ',', '.') }} / mes</td>
+            </tr>
+            <tr>
+              <td>Coste Ingenieros de Mantenimiento:</td>
+              <td>{{ number_format($sede->ingenieros * 30000, 0, ',', '.') }}€ / mes</td>
+            </tr>
+            <tr>
+              <td>Costes totales:</td>
+              <td>280.000€ / mes(inop)</td>
+            </tr>
+          </table>
         </div>
       </div>
       
