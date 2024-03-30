7596219b85aa446860c6ed6f73c456b2ba6719f8
CarlosOlivaAnar <carlosoliva5503@gmail.com>
2024-03-30 17:46:59 +0100

    Imagen cont exto landing

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -26,23 +26,34 @@
         <div class="resumen">
             <ul>
               <li>
-                <i class="bx bx-building"></i>
-                <h3>Comprar Hangar</h3>
+                <i class="bx bx-stats"></i>
+                <h3>Demanda</h3>
+                <h4>Cada aeropuerto tiene su propia demanda y pasajeros simulados segun el destino y origen de la ruta</h4>
               </li>
               <li>
-                <i class="bx bx-user-plus"></i>
-                <h3>Contratar Ingenieros</h3>
+                <i class="bx bx-group"></i>
+                <h3>Multijugador</h3>
+                <h4>El mundo en el que se gestiona tu compañia aerea esta en constante competencia con el resto de jugadores de la aplicacion, afectando a la demanda de tus vuelos y tus ingresos</h4>
               </li></a>
               <li>
-                <i class="bx bx-trending-up"></i>
-                <h3>Mejoras</h3>
+                <i class="bx bxs-badge-dollar"></i>
+                <h3>Gratuito</h3>
+                <h4>Este proyecto esta hecho con el fin de ser 100% gratuito sin anuncios ni microtransacciones o mecanicas pay to win. El proyecto esta financiado completamente a base de donaciones de los usuarios</h4>
               </li>
               <li>
-                <i class="bx bx-trending-up"></i>
-                <h3>Mejoras</h3>
+                <i class="bx bx-network-chart"></i>
+                <h3>Monopolio</h3>
+                <h4>Emplea tecnicas no eticas sobre otros jugadores para ser la compañia aerea mejor valorada (como en el mundo real vamos)</h4>
               </li>
             </ul>
           </div>
+          <div class="imagenConTexto" style="background-image: url('{{ asset('images/emiratesFondo.jpg') }}');">
+            <h3>COMIENZA AQUI TU COMPAÑIA AEREA</h3>
+            <a href="{{ route('register') }}">Registrarse</a>
+          </div>
     </main>
+    <footer>
+      TEXTO DE EJEMPLO
+    </footer>
 </body>
 </html>
