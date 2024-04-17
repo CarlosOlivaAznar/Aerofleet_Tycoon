8ca30d120bf81d1099559f5072031002ec87d6fa
CarlosOlivaAnar <carlosoliva5503@gmail.com>
2024-04-17 11:24:34 +0200

    consultar rutas de otros usuarios

--- app/Http/Controllers/CompetenciaController.php
+++ app/Http/Controllers/CompetenciaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Aeropuerto;
+use App\Models\Espacio;
+use App\Models\Ruta;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -18,7 +20,23 @@ class CompetenciaController extends Controller
 
     public function demandaRuta(Request $request)
     {
-        return view('competencia.demandaRuta');
+        $origen = $request->origenHid;
+        $destino = $request->destinoHid;
+        
+        if($origen != null && $destino != null){
+            $grupoRutas = Ruta::join('espacios as e', 'rutas.espacio_departure_id', '=', 'e.id')
+            ->join('espacios as es', 'rutas.espacio_arrival_id', '=', 'es.id')
+            ->where('e.aeropuerto_id', $origen)
+            ->where('es.aeropuerto_id', $destino)
+            ->select('rutas.*')
+            ->get()->groupBy('user_id');
+            
+            return view('competencia.demandaRuta', ['grupoRutas' => $grupoRutas]);
+        } else {
+
+            return redirect()->route('competencia.index');
+        }
+        
     }
 
     public function rutasCompetencia()
--- resources/views/competencia/demandaRuta.blade.php
+++ resources/views/competencia/demandaRuta.blade.php
@@ -25,6 +25,78 @@
           </ul>
         </div>
       </div>
+      
+      @if (count($grupoRutas) > 0)
+      @foreach ($grupoRutas as $rutas)
+      <div class="tablas">
+        <div class="cabecera">
+          <i class="bx bx-outline"></i>
+          <h3>Rutas del la compañia {{ $rutas[0]->user->nombreCompanyia }}</h3>
+          @if ($rutas[0]->flota->estado == 0 || $rutas[0]->flota->estado == 2)
+              <span class="rojo">RUTA INACTIVA</span>
+          @endif
+          <div class="botones-tablas">
+            @if ($rutas[0]->flota->estado == 0)
+            <a href="{{ route('flota.activarRuta', ['id' => $rutas[0]->flota->id]) }}" class="boton">
+              <i class="bx bx-check-square" ></i>
+              <span>Activar ruta</span>
+            </a>
+            @endif
+            <a href="{{ route('rutas.crearRutaAvion', ['id' => $rutas[0]->flota->id]) }}" class="boton">
+              <i class="bx bx-add-to-queue"></i>
+              <span>Crear ruta</span>
+            </a>
+          </div>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Avion</th>
+              <th>Matricula</th>
+              <th>Origen</th>
+              <th>Destino</th>
+              <th>Distancia</th>
+              <th>Tiempo</th>
+              <th>Hora de salida</th>
+              <th>Hora de llegada</th>
+              <th>Precio Billete</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($rutas as $ruta)
+            <tr>
+              <td><i class="bx"><img src="{{ asset($ruta->flota->avion->img) }}" alt=""></i></td>
+              <td>{{ $ruta->flota->matricula }}</td>
+              <td>{{ $ruta->espacio_departure->aeropuerto->icao }}</td>
+              <td>{{ $ruta->espacio_arrival->aeropuerto->icao }}</td>
+              <td>{{ $ruta->distancia }}km</td>
+              <td>{{ $ruta->tiempoEstimado }}</td>
+              <td>{{ $ruta->horaInicio }}</td>
+              <td>{{ $ruta->horaFin }}</td>
+              <td>{{ $ruta->precioBillete }}€</td>
+              <td>
+                <a class="vender tooltip" href="{{ route('rutas.borrarRuta', ['id' => $ruta->id]) }}">
+                  <i class="bx bx-trash"></i>
+                  <span class="tooltiptext">Eliminar Ruta</span>
+                </a>
+                <a class="modificar tooltip" data-modal-target="modalAvion{{ $ruta->id }}">
+                  <i class="bx bx-wrench"></i>
+                  <span class="tooltiptext">Modificar Ruta</span>
+                </a>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+      @endforeach  
+      @else
+          <div class="mensaje">
+            <i class="bx bx-error"></i>
+            <h4>No existen rutas creadas con los parametros elegidos</h4>
+          </div>
+      @endif
 
       
     </main>
