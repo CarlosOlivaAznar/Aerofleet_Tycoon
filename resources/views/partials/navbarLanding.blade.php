34f8b6cdab754f7a7cf2ce433695007cfa02c8d0
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-04-25 11:59:33 +0200

    link al landing y ocultar titulo en vista de movil

--- resources/views/partials/navbarLanding.blade.php
+++ resources/views/partials/navbarLanding.blade.php
@@ -1,5 +1,5 @@
 <nav class="barra-login">
-    <a href="#" class="titulo-pag">
+    <a href="{{ route('landing.landing') }}" class="titulo-pag">
         <img class="icono" src="{{ asset('images/logos/logo_AFT_100px_icon.png') }}" alt="AFT">
         <div class="titulo-nombre">Aero<span>Fleet</span></div>
     </a>
