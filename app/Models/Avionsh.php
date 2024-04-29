42e88119d8cd862653a98cbc1acbc844e88984c0
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-04-29 12:03:15 +0200

    aviones nuevos de segunda mano

--- app/Models/Avionsh.php
+++ app/Models/Avionsh.php
@@ -50,8 +50,41 @@ class Avionsh extends Model
             [14, 'images/sh/a350_airCaribes.png', 'Air Caribes'],
             [15, 'images/sh/a380_british.png', 'British Airways'],
             [15, 'images/sh/a380_malaysia.png', 'Malaysia Airlines'],
+            [4, 'images/sh/a320neo_india.png', 'Air India'],
+            [4, 'images/sh/a320neo_ana.png', 'All Nippon Airways'],
+            [4, 'images/sh/a320neo_malta.png', 'Air Malta'],
+            [4, 'images/sh/a320neo_avianca.png', 'Avianca'],
+            [4, 'images/sh/a320neo_eurowings.png', 'Eurowings'],
+            [4, 'images/sh/a320neo_spirit.png', 'Spirit'],
+            [14, 'images/sh/a350_cathay.png', 'Cathay Pacific'],
+            [14, 'images/sh/a350_qatar.png', 'Qatar Airways'],
+            [14, 'images/sh/a350_sas.png', 'Scandinavian Airlines'],
+            [14, 'images/sh/a350_thai.png', 'Thai Airways International'],
+            [23, 'images/sh/e170_hop.png', 'Air France Hop'],
+            [23, 'images/sh/e170_alitalia.png', 'Alitalia Express'],
+            [24, 'images/sh/e175_alaska.png', 'Alaska Airlines'],
+            [24, 'images/sh/e175_aeromexico.png', 'AeroMexico Connect'],
+            [25, 'images/sh/e190_aircanada.png', 'Air Canada'],
+            [25, 'images/sh/e190_ba.png', 'BA CityFlyer'],
+            [25, 'images/sh/e190_finnair.png', 'Finnair'],
+            [26, 'images/sh/e195_flybe.png', 'FlyBe'],
+            [26, 'images/sh/e195_binter.png', 'Binter'],
+            [26, 'images/sh/e195_azul.png', 'Azul Linhas Aéreas'],
+            [27, 'images/sh/crj200_delta.png', 'Delta ExpressJet'],	
+            [27, 'images/sh/crj200_sa.png', 'SA Express Airways'],
+            [28, 'images/sh/crj700_airfrance.png', 'Air France HOP'],
+            [28, 'images/sh/crj700_united.png', 'United Express'],
+            [29, 'images/sh/crj900_sas.png', 'Scandinavian Airlines'],
+            [29, 'images/sh/crj900_american.png', 'American Eagle'],
+            [29, 'images/sh/crj900_aircanada.png', 'Air Canada Express'],
+            [30, 'images/sh/crj1000_airnostrum.png', 'Iberia Regional Air Nostrum'],
+            [30, 'images/sh/crj1000_binter.png', 'Binter'],
+            [30, 'images/sh/crj1000_hop.png', 'Air France HOP'],
         ];
 
+        // Plantilla avion nuevo
+        // [30, 'images/sh/', ''],		          
+        
         return $modelosSh[rand(0, count($modelosSh)-1)];
     }
 }
