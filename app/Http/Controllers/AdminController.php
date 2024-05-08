674ce934d25a59acefd289031656477b26e01176
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-05-08 17:16:30 +0200

    pagina admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $users = User::all();
+        return view('admin.index', ['users' => $users]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -17,11 +17,49 @@
     <main>
       <div class="cabecera">
         <div class="titulo">
-          <h1>{{ __('competence.competence') }}</h1>
+          <h1>{{ __('admin.title') }}</h1>
         </div>
       </div>
 
-
+      <div class="tablas">
+        <div class="cabecera">
+            <i class="bx bx-user"></i>
+            <h3>{{ __('admin.users') }}</h3>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>{{ __('admin.airlineName') }}</th>
+                    <th>{{ __('admin.name') }}</th>
+                    <th>{{ __('admin.email') }}</th>
+                    <th>{{ __('admin.userType') }}</th>
+                    <th>{{ __('admin.peferredLang') }}</th>
+                    <th>{{ __('admin.balance') }}</th>
+                    <th>{{ __('admin.lastConnected') }}</th>
+                    <th>{{ __('admin.actions') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+              @foreach ($users as $user)
+              <tr>
+                <td>{{ $user->nombreCompanyia }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>
+                    @if ($user->tipoUsuario === 1)
+                        {{ __('admin.userType1') }}
+                    @else
+                        {{ __('admin.userType0') }}
+                    @endif
+                </td>
+                <td>{{ $user->preferred_language }}</td>
+                <td>{{ $user->saldo }}</td>
+                <td>{{ $user->ultimaConexion }}</td>
+              </tr> 
+              @endforeach
+            </tbody>
+        </table>
+      </div>
 
     </main>
   </div>
