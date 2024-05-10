2fe5654f1ec074160110f445614918f64faf1969
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-05-10 13:07:04 +0200

    trusted proxies cambiado al de la base de datos

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies  = "*";
+    protected $proxies  = "10.106.0.0/20";
 
     /**
      * The headers that should be used to detect proxies.
