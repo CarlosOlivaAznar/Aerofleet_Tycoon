dfbcee9aaa7e5c1335ee638f4a5154da11f0ec3e
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-07-29 19:50:04 +0200

    selector de idiomas

--- resources/views/partials/navbarLanding.blade.php
+++ resources/views/partials/navbarLanding.blade.php
@@ -4,6 +4,10 @@
         <div class="titulo-nombre">Aero<span>Fleet</span></div>
     </a>
     <div class="login">
+        <div class="lang">
+            <img src="{{ asset('icons/flags/en.png') }}" alt="flag">
+            <i class='bx bxs-down-arrow'></i>
+        </div>
         <div><a href="{{ route('login') }}">{{ __('landing.signIn') }}</a></div>
         <div><a class="login-registrarse" href="{{ route('register') }}">{{ __('landing.signUp') }}</a></div>
     </div>
