5d7d2d07939176240596b750f989ae251295bafd
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-08-01 11:16:01 +0200

    selector de idiomas

--- resources/views/partials/navbarLanding.blade.php
+++ resources/views/partials/navbarLanding.blade.php
@@ -5,8 +5,21 @@
     </a>
     <div class="login">
         <div class="lang">
-            <img src="{{ asset('icons/flags/en.png') }}" alt="flag">
-            <i class='bx bxs-down-arrow'></i>
+            <button class="langbtn">
+                <img src="{{ asset('icons/flags/es.png') }}" alt="flag-lang">
+                ES
+                <i class="bx bxs-down-arrow"></i>
+            </button>
+            <div class="lang-content">
+              <a href="#">
+                <img src="{{ asset('icons/flags/en.png') }}" alt="flag-lang">
+                EN
+              </a>
+              <a href="#">
+                <img src="{{ asset('icons/flags/es.png') }}" alt="flag-lang">
+                ES
+              </a>
+            </div>
         </div>
         <div><a href="{{ route('login') }}">{{ __('landing.signIn') }}</a></div>
         <div><a class="login-registrarse" href="{{ route('register') }}">{{ __('landing.signUp') }}</a></div>
