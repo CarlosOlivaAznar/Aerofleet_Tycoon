c4dbecbb5c20ae3db2b1a136398bd580b978f2c9
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-08-28 20:26:47 +0200

    changelog V-0.3.2

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -46,6 +46,24 @@
             <hr>
 
             <div class="collapse-content" id="collapse-content">
+              <h4>V-0.3.2</h4>
+              <ul>
+                <li>Ahora se muestra información adicional de los aviones en flota y al comprar aviones</li>
+                <li>Bugfix donde el cuadro de los mensajes se mostraba de manera erronea</li>
+                <li>Bugfix donde el gráfico de saldo no se mostraba correctamente en dispositivos móviles</li>
+                <li>Bugfix ahora los gastos de los aviones en información se muestra con 2 decimales máximo</li>
+                <li>Al activar rutas ahora se tiene que esperar minimo un dia para que la ruta pase a estar activa y generar ingresos</li>
+                <li>Al comprar un nuevo avión se mostrará un mensaje de confirmación de compra</li>
+                <li>Los aviones con mayor antigüedad serán más caros de mantener y se cobrara un plus por reparacion</li>
+                <li>Los buscadores ahora funcionan correctamente con los mousepad de los portátiles</li>
+                <li>Los aviones ahora tienen restricciones al aterrizar en los aeropuertos segun la categoria que tenga</li>
+                <li>Ya no se pueden crear rutas que tengan hora de salida a las 24:00z</li>
+                <li>Ahora se muestra información del aeropuerto seleccionado al comprar espacios</li>
+                <li>Ahora en competencia se puede consultar el ranking de usuarios en la aplicación</li>
+                <li>Añadido modo oscuro</li>
+                <li>Añadido selector de idiomas en la página de cuenta como en la barra de navegación</li>
+              </ul>
+
               <h4>V-0.3.1</h4>
               <ul>
                 <li>Pagina del tutorial adaptada a moviles</li>
