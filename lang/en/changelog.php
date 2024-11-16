09cf2ad5c66cc36e5c9283b279024358fce1bcbd
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-11-16 11:48:31 +0100

    mostrar changelog traducido

---
+++ lang/en/changelog.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    'changelog' => '<h4>V-0.3.2</h4>
+              <ul>
+                <li>Additional information about aircraft in the fleet and when purchasing planes is now displayed</li>
+                <li>Bugfix where the message box was displayed incorrectly</li>
+                <li>Bugfix where the balance chart was not displayed correctly on mobile devices</li>
+                <li>Bugfix where aircraft expenses in the information section are now shown with a maximum of 2 decimals</li>
+                <li>When activating routes, you now have to wait at least one day for the route to become active and generate income</li>
+                <li>A confirmation message will now be displayed when purchasing a new aircraft</li>
+                <li>Older aircraft will be more expensive to maintain, and a repair surcharge will be applied</li>
+                <li>Search functions now work correctly with laptop touchpads</li>
+                <li>Aircraft now have restrictions when landing at airports based on their category</li>
+                <li>It is no longer possible to create routes with departure times at 24:00z</li>
+                <li>Information about the selected airport is now displayed when purchasing slots</li>
+                <li>The competition section now allows users to view the rankings in the application</li>
+                <li>Dark mode added</li>
+                <li>Language selector added to the account page and navigation bar</li>
+              </ul>
+
+              <h4>V-0.3.1</h4>
+              <ul>
+                <li>Tutorial page adapted for mobile devices</li>
+                <li>Added a search bar when purchasing slots</li>
+                <li>Fixed an error where you could purchase 0 slots</li>
+                <li>You can now change your company name from the headquarters page</li>
+                <li>The ICAO code is now displayed when creating a route</li>
+              </ul>
+
+              <h4>V-0.3.0</h4>
+              <ul>
+                <li>Airport slots are now limited and only a maximum number can be purchased among all players</li>
+                <li>Added Embraer and Bombardier brands</li>
+                <li>Added new aircraft models from Airbus and Boeing</li>
+                <li>Login pages adapted for mobile devices</li>
+                <li>Added competition page</li>
+                <li>Tooltips added to buttons without text</li>
+                <li>Aircraft capacity added in the Fleet section</li>
+                <li>Pages adapted for mobile</li>
+                <li>Route status added: On the ground/In flight/In maintenance</li>
+                <li>When creating a route, available slot options are displayed</li>
+                <li>Added alert and information messages about routes on the home page</li>
+                <li>Added a map in the purchase slots section with information about available airports</li>
+              </ul>
+
+              <h4>V-0.2.0</h4>
+              <ul>
+                <li>Long-distance flights now have a distance bonus</li>
+                <li>The map shows data about planes, routes, and airports</li>
+                <li>Ticket prices can now be modified</li>
+                <li>Routes are displayed in groups and divided by aircraft</li>
+                <li>Aircraft can be sent for maintenance</li>
+                <li>Tables can now be viewed on mobile devices</li>
+                <li>Aircraft registrations are generated automatically</li>
+              </ul>',
+];
+
+// Plantulla
+// '' => '',
---
+++ lang/es/changelog.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    'changelog' => '<h4>V-0.3.2</h4>
+              <ul>
+                <li>Ahora se muestra información adicional de los aviones en flota y al comprar aviones</li>
+                <li>Bugfix donde el cuadro de los mensajes se mostraba de manera erronea</li>
+                <li>Bugfix donde el gráfico de saldo no se mostraba correctamente en dispositivos móviles</li>
+                <li>Bugfix ahora los gastos de los aviones en información se muestra con 2 decimales máximo</li>
+                <li>Al activar rutas ahora se tiene que esperar minimo un dia para que la ruta pase a estar activa y generar ingresos</li>
+                <li>Al comprar un nuevo avión se mostrará un mensaje de confirmación de compra</li>
+                <li>Los aviones con mayor antigüedad serán más caros de mantener y se cobrara un plus por reparacion</li>
+                <li>Los buscadores ahora funcionan correctamente con los mousepad de los portátiles</li>
+                <li>Los aviones ahora tienen restricciones al aterrizar en los aeropuertos segun la categoria que tenga</li>
+                <li>Ya no se pueden crear rutas que tengan hora de salida a las 24:00z</li>
+                <li>Ahora se muestra información del aeropuerto seleccionado al comprar espacios</li>
+                <li>Ahora en competencia se puede consultar el ranking de usuarios en la aplicación</li>
+                <li>Añadido modo oscuro</li>
+                <li>Añadido selector de idiomas en la página de cuenta como en la barra de navegación</li>
+              </ul>
+
+              <h4>V-0.3.1</h4>
+              <ul>
+                <li>Pagina del tutorial adaptada a moviles</li>
+                <li>En comprar espacios ahora tiene un buscador</li>
+                <li>Arreglado un error donde podrias comprar 0 espacios</li>
+                <li>Ahora puedes cambiar tu nombre de la compañia desde la pagina sede</li>
+                <li>Ahora se muestra el codigo ICAO en crear ruta</li>
+              </ul>
+
+              <h4>V-0.3.0</h4>
+              <ul>
+                <li>Ahora los espacios de los aeropuertos son limitados y solo se pueden comprar un numero maximo entre todos los jugadores</li>
+                <li>Añadidos las marcas de Embraer y Bombardier</li>
+                <li>Añadido nuevos modelos de aviones de Airbus y Boeing</li>
+                <li>Paginas de login adaptadas a dispositivos moviles</li>
+                <li>Añadido la pagina de competencia</li>
+                <li>Añadido tooltips en los botones que no tienen texto</li>
+                <li>Añadido la capacidad del avion en Flota</li>
+                <li>Paginas adaptadas a movil</li>
+                <li>Añadido el estado de las rutas En tierra/En ruta/En mantenimiento</li>
+                <li>Al crear la ruta te muestra las opciones de los espacios que tienes disponibles</li>
+                <li>Añadido a la pagina home mensajes de alerta y mensajes de informacion de las rutas</li>
+                <li>Añadido un mapa en comprar espacios con la informacion de los aeropuertos disponibles</li>
+              </ul>
+
+              <h4>V-0.2.0</h4>
+              <ul>
+                <li>Los vuelos de larga distancia ahora tienen un plus por distancia</li>
+                <li>El mapa muestra los datos de los aviones, rutas y aeropuertos</li>
+                <li>Ahora se puede modificar el precio de los billetes</li>
+                <li>Las rutas se muestran en grupo y dividido por aviones</li>
+                <li>Los aviones se pueden mandar a mantenimiento</li>
+                <li>Las tablas se pueden ver ahora en dispositivos moviles</li>
+                <li>Las matriculas de los aviones se generan automaticamente</li>
+              </ul>',
+];
+
+// Plantulla
+// '' => '',
