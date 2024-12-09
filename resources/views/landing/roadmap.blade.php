b48b59bd2625110e4cc7cbe42a26e32235cbb74a
S-1-5-32-544 <carlosoliva5503@gmail.com>
2024-12-09 10:33:09 +0100

    vista roadmap adaptada a moviles

--- resources/views/landing/roadmap.blade.php
+++ resources/views/landing/roadmap.blade.php
@@ -15,17 +15,33 @@
                     <h2>{{ __('landing.version2') }}</h2>
                     <p>{{ __('landing.version2txt') }}</p>
                 </div>
+                <div class="side-card left movil">
+                    <h2>{{ __('landing.version3') }}</h2>
+                    <p>{{ __('landing.version3txt') }}</p>
+                </div>
                 <div class="side-card left">
                     <h2>{{ __('landing.version4') }}</h2>
                     <p>{{ __('landing.version4txt') }}</p>
+                </div>
+                <div class="side-card left movil">
+                    <h2>{{ __('landing.version5') }}</h2>
+                    <p>{{ __('landing.version5txt') }}</p>
                 </div>
                 <div class="side-card left">
                     <h2>{{ __('landing.version6') }}</h2>
                     <p>{{ __('landing.version6txt') }}</p>
                 </div>
+                <div class="side-card left movil">
+                    <h2>{{ __('landing.version7') }}</h2>
+                    <p>{{ __('landing.version7txt') }}</p>
+                </div>
                 <div class="side-card left">
                     <h2>{{ __('landing.version10') }}</h2>
                     <p>{{ __('landing.version10txt') }}</p>
+                </div>
+                <div class="side-card left movil">
+                    <h2>{{ __('landing.versionx') }}</h2>
+                    <p>{{ __('landing.versionxtxt') }}</p>
                 </div>
             </div>
             <div><img src="{{ asset('images/roadMap.png') }}" alt="roadMapImage"></div>
