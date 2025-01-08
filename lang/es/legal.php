8c7e80cb8392d8646434749ba81ddd733ae92342
S-1-5-32-544 <carlosoliva5503@gmail.com>
2025-01-08 10:57:08 +0100

    politica de privacidad y terminos y condiciones traducido

---
+++ lang/es/legal.php
@@ -0,0 +1,148 @@
+<?php
+
+return [
+
+    'termsAndConditions' => '<p>Fecha de Efecto: 10/04/2024</p><br>
+
+            <p>Por favor, lea atentamente estos términos y condiciones de uso ("Términos", "Términos y
+                Condiciones") antes de acceder y utilizar nuestra aplicación web.
+            </p><br>
+
+            <p>
+                1. Aceptación de los Términos
+                Al acceder o utilizar la aplicación web, usted acepta estar legalmente vinculado por estos Términos. Si
+                no está de acuerdo con alguna parte de los términos, no podrá acceder a la aplicación web.
+            </p><br>
+
+            <p>
+                2. Uso de la Aplicación Web
+                La aplicación web, incluidos todos sus contenidos, es propiedad de Carlos Oliva Aznar o sus
+                licenciantes. Está destinada únicamente para fines de entretenimiento. Usted acepta no utilizar la
+                aplicación web para ningún propósito ilegal o no autorizado.
+            </p><br>
+
+            <p>
+                3. Propiedad Intelectual
+                Todos los derechos de propiedad intelectual de la aplicación web, incluyendo, pero no limitado a los
+                nombres, logotipos y contenido, son propiedad de Carlos Oliva Aznar o de terceros proveedores de
+                contenido. Usted acepta no utilizar, reproducir o distribuir cualquier contenido de la aplicación web sin
+                autorización.
+            </p><br>
+
+            <p>
+                4. Enlaces a Terceros
+                La aplicación web puede contener enlaces a sitios web de terceros que no están bajo nuestro control.
+                No somos responsables del contenido, políticas de privacidad u otras prácticas de cualquier sitio web
+                de terceros al que se acceda a través de la aplicación web.
+            </p><br>
+
+            <p>
+                5. Donaciones
+                Apreciamos sinceramente cualquier donación que pueda hacer para ayudar a mantener y mejorar
+                nuestra aplicación web. Las donaciones se procesarán a través de PayPal y son completamente
+                voluntarias. Las donaciones se utilizarán únicamente para apoyar el desarrollo y mantenimiento
+                continuo de la aplicación web.
+            </p><br>
+
+            <p>
+                6. Privacidad
+                Respetamos su privacidad y protegemos sus datos personales de acuerdo con nuestra Política de
+                Privacidad. Al utilizar la aplicación web, usted acepta nuestras prácticas de privacidad.
+            </p><br>
+
+            <p>
+                7. Limitación de Responsabilidad
+                En ningún caso seremos responsables ante usted por cualquier daño directo, indirecto, incidental,
+                especial, consecuente o punitivo que surja del uso de la aplicación web.
+            </p><br>
+            
+            <p>
+                8. Modificaciones
+                Nos reservamos el derecho de modificar o reemplazar estos Términos en cualquier momento. Se le
+                notificarán los cambios significativos antes de que entren en vigor.
+            </p><br>
+
+            <p>
+                9. Ley Aplicable
+                Estos Términos se regirán e interpretarán de acuerdo con las leyes del país de origen de la aplicación
+                web, sin tener en cuenta sus disposiciones sobre conflictos de leyes.
+            </p><br>
+
+            <p>
+                10. Contacto
+                Si tiene alguna pregunta sobre estos Términos, puede ponerse en contacto con nosotros a través de
+                los medios proporcionados en la aplicación web.
+            </p><br>
+
+            <p>
+                Fecha de Efecto: 10/04/2024
+            </p><br>',
+    'privacyPolicy' => '<p>Fecha de Efecto: 10/04/2024</p><br>
+
+            <p>
+                Gracias por utilizar nuestra aplicación web. Esta política de privacidad explica cómo recopilamos,
+                utilizamos y protegemos la información personal que usted nos proporciona.
+            </p><br>
+
+            <p>
+                1. Información Recopilada
+                La única información personal que recopilamos es su nombre de usuario, contraseña y correo
+                electrónico. Esta información es necesaria para proporcionarle acceso a nuestra aplicación web y para
+                identificarlo de manera única dentro del sistema.
+            </p><br>
+
+            <p>
+                2. Uso de la Información
+                Utilizamos su información personal únicamente con el propósito de proporcionarle acceso y mantener
+                la funcionalidad de nuestra aplicación web. No compartimos, vendemos ni alquilamos su información
+                personal a terceros.
+            </p><br>
+
+            <p>
+                3. Cookies y Rastreo
+                No utilizamos cookies ni ningún otro método de rastreo para recopilar información sobre su actividad
+                en nuestra aplicación web. Respetamos su privacidad y no realizamos seguimiento de su
+                comportamiento en línea.
+                En diferentes apartados de la web se utilizan cookies de sesión para mejorar la experiencia de la
+                aplicación.
+            </p><br>
+
+            <p>
+                4. Seguridad de la Información
+                Tomamos medidas de seguridad razonables para proteger su información personal contra el acceso no
+                autorizado, la divulgación, la alteración o la destrucción. Sin embargo, tenga en cuenta que ningún
+                método de transmisión a través de Internet o método de almacenamiento electrónico es 100% seguro.
+            </p><br>
+            
+            <p>
+                5. Acceso y Corrección de la Información
+                Usted tiene derecho a acceder y corregir su información personal en cualquier momento. Puede
+                actualizar su nombre de usuario, contraseña o correo electrónico en la configuración de su cuenta
+                dentro de la aplicación web.
+            </p><br>
+
+            <p>
+                6. Consentimiento
+                Al utilizar nuestra aplicación web, usted acepta la recopilación y el uso de su información personal de
+                acuerdo con esta política de privacidad.
+            </p><br>
+
+            <p>
+                7. Cambios en la Política de Privacidad
+                Nos reservamos el derecho de modificar esta política de privacidad en cualquier momento. Cualquier
+                cambio significativo será notificado a través de la aplicación web o por otros medios apropiados.
+            </p><br>
+
+            <p>
+                8. Contacto
+                Si tiene alguna pregunta sobre esta política de privacidad, puede ponerse en contacto con nosotros a
+                través de los medios proporcionados en la aplicación web.
+            </p><br>
+
+            <p>
+                Fecha de Efecto: 10/04/2024
+            </p><br>',
+];
+
+// Plantilla
+// '' => '',
--- resources/views/landing/terminosCondiciones.blade.php
+++ resources/views/landing/terminosCondiciones.blade.php
@@ -6,84 +6,10 @@
 <body>
     @include('partials.navbarLanding')
     <main class="mainContent">
-        <h1>Términos y condiciones de uso</h1><br>
+        <h1>{{ __('landing.termns') }}</h1><br>
 
         <div class="changelog" style="padding: 25px 30px">
-            <p>Fecha de Efecto: 10/04/2024</p><br>
-
-            <p>Por favor, lea atentamente estos términos y condiciones de uso ("Términos", "Términos y
-                Condiciones") antes de acceder y utilizar nuestra aplicación web.
-            </p><br>
-
-            <p>
-                1. Aceptación de los Términos
-                Al acceder o utilizar la aplicación web, usted acepta estar legalmente vinculado por estos Términos. Si
-                no está de acuerdo con alguna parte de los términos, no podrá acceder a la aplicación web.
-            </p><br>
-
-            <p>
-                2. Uso de la Aplicación Web
-                La aplicación web, incluidos todos sus contenidos, es propiedad de Carlos Oliva Aznar o sus
-                licenciantes. Está destinada únicamente para fines de entretenimiento. Usted acepta no utilizar la
-                aplicación web para ningún propósito ilegal o no autorizado.
-            </p><br>
-
-            <p>
-                3. Propiedad Intelectual
-                Todos los derechos de propiedad intelectual de la aplicación web, incluyendo, pero no limitado a los
-                nombres, logotipos y contenido, son propiedad de Carlos Oliva Aznar o de terceros proveedores de
-                contenido. Usted acepta no utilizar, reproducir o distribuir cualquier contenido de la aplicación web sin
-                autorización.
-            </p><br>
-
-            <p>
-                4. Enlaces a Terceros
-                La aplicación web puede contener enlaces a sitios web de terceros que no están bajo nuestro control.
-                No somos responsables del contenido, políticas de privacidad u otras prácticas de cualquier sitio web
-                de terceros al que se acceda a través de la aplicación web.
-            </p><br>
-
-            <p>
-                5. Donaciones
-                Apreciamos sinceramente cualquier donación que pueda hacer para ayudar a mantener y mejorar
-                nuestra aplicación web. Las donaciones se procesarán a través de PayPal y son completamente
-                voluntarias. Las donaciones se utilizarán únicamente para apoyar el desarrollo y mantenimiento
-                continuo de la aplicación web.
-            </p><br>
-
-            <p>
-                6. Privacidad
-                Respetamos su privacidad y protegemos sus datos personales de acuerdo con nuestra Política de
-                Privacidad. Al utilizar la aplicación web, usted acepta nuestras prácticas de privacidad.
-            </p><br>
-
-            <p>
-                7. Limitación de Responsabilidad
-                En ningún caso seremos responsables ante usted por cualquier daño directo, indirecto, incidental,
-                especial, consecuente o punitivo que surja del uso de la aplicación web.
-            </p><br>
-            
-            <p>
-                8. Modificaciones
-                Nos reservamos el derecho de modificar o reemplazar estos Términos en cualquier momento. Se le
-                notificarán los cambios significativos antes de que entren en vigor.
-            </p><br>
-
-            <p>
-                9. Ley Aplicable
-                Estos Términos se regirán e interpretarán de acuerdo con las leyes del país de origen de la aplicación
-                web, sin tener en cuenta sus disposiciones sobre conflictos de leyes.
-            </p><br>
-
-            <p>
-                10. Contacto
-                Si tiene alguna pregunta sobre estos Términos, puede ponerse en contacto con nosotros a través de
-                los medios proporcionados en la aplicación web.
-            </p><br>
-
-            <p>
-                Fecha de Efecto: 10/04/2024
-            </p><br>
+            {!! __('legal.termsAndConditions') !!}
         </div>
     </main>
 
